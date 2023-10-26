9c5981bacc6e984a753d9d9fa5dc340c73d8c732
Zildan <muhamadzildan13@gmail.com>
2023-10-26 23:54:29 +0700

    nav-bar

--- resources/views/components/head/head.blade.php
+++ resources/views/components/head/head.blade.php
@@ -2,6 +2,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,700;1,9..40,100;1,9..40,400;1,9..40,500;1,9..40,700&display=swap" rel="stylesheet">
+   
     <title>Beranda | Info Cuaca</title>
 
     {{-- Style Css yang di letakan di dalam folder public yang sudah saya siapakan --}}
--- resources/views/components/navbar/navbar.blade.php
+++ resources/views/components/navbar/navbar.blade.php
@@ -1,1 +1,14 @@
 {{-- Isi hanya dengan navbar saja --}}
+<header class="nav-bar">
+    <div class="nav-bar_brand">
+        <h1> Info Cuaca </h1>
+    </div>
+    <nav class="nav-bar_navigation">
+        <ul>
+            <li><a href="#">Weather</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Radar</a></li>
+            <li><a href="#">Air Quality</a></li>
+        </ul>  
+    </nav>
+</header>
