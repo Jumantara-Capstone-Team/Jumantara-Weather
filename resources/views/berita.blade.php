5f4882a0484fcc6896c6be5d87fd77db90eed2bf
Syaiful Imanudin <yusriltc@gmail.com>
2023-11-06 11:02:02 +0700

    add page berita.blade.php

---
+++ resources/views/berita.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>News | Info Cuaca</title>
+
+@include('components.head.head')
+@include('components.navbar.navbar')
+
+<body>
+
+
+</body>
+@include('components.footer.footer')
+
+<script src="{{ asset('js/main.js') }}"></script>
+
+</html>
