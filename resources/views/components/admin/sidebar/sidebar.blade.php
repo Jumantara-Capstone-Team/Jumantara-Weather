76b31e5d1fad84498a8dcde9fc90b7807582ed1f
Syaiful Imanudin <yusriltc@gmail.com>
2023-11-11 00:04:15 +0700

    edit sidebar

--- resources/views/components/admin/sidebar/sidebar.blade.php
+++ resources/views/components/admin/sidebar/sidebar.blade.php
@@ -1,83 +1,78 @@
- <!-- Sidebar -->
- <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
-
-     <!-- Sidebar - Brand -->
-     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-         <div class="sidebar-brand-text mx-3">Admin </div>
-     </a>
-
-     <!-- Divider -->
-     <hr class="sidebar-divider my-0">
-
-     <!-- Nav Item - Dashboard -->
-     <li class="nav-item">
-         <a class="nav-link" href="/dashboard">
-             <i class="fas fa-fw fa-tachometer-alt"></i>
-             <span>Dashboard</span></a>
-     </li>
-
-     <!-- Divider -->
-     <hr class="sidebar-divider">
-
-     <!-- Heading -->
-     <div class="sidebar-heading">
-         Edit Content
-     </div>
-
-     <!-- Nav Item - Pages Collapse Menu -->
-     <li class="nav-item">
-         <a class="nav-link collapsed" href="/index" data-toggle="collapse" data-target="#collapseOne"
-             aria-expanded="true" aria-controls="collapseOne">
-             <i class="fa fa-clone"></i>
-             <span>Add News</span>
-         </a>
-         <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
-             <div class="bg-white py-2 collapse-inner rounded">
-                 <h6 class="collapse-header">Add News</h6>
-                 <a class="collapse-item" href="/dashboard/posts">Add Berita</a>
-             </div>
-         </div>
-     </li>
-
-     <!-- Divider -->
-     <hr class="sidebar-divider">
-
-     <!-- Heading -->
-     <div class="sidebar-heading">
-         Addons
-     </div>
-
-     <!-- Nav Item - Pages Collapse Menu -->
-     <li class="nav-item">
-         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-             aria-expanded="true" aria-controls="collapsePages">
-             <i class="fas fa-fw fa-folder"></i>
-             <span>Menu Navigasi</span>
-         </a>
-         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-             <div class="bg-white py-2 collapse-inner rounded">
-                 <h6 class="collapse-header">Login Screens:</h6>
-                 <form method="POST" action="{{ route('logout') }}">
-                     @csrf
-                     <button class="collapse-item custom-button" type="submit">Logout</button>
-                 </form>
-                 <h6 class="collapse-header">Other Pages:</h6>
-                 <a class="collapse-item" href="/">Back To Beranda</a>
-                 <div class="collapse-divider"></div>
-             </div>
-         </div>
-     </li>
-
-     <!-- Divider -->
-     <hr class="sidebar-divider d-none d-md-block">
-
-     <!-- Sidebar Toggler (Sidebar) -->
-     <div class="text-center d-none d-md-inline">
-         <button class="rounded-circle border-0" id="sidebarToggle"></button>
-     </div>
-
-     <!-- Sidebar Message -->
+<ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
 
 
- </ul>
- <!-- End of Sidebar -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+        <div class="sidebar-brand-text mx-3">Admin </div>
+    </a>
+
+
+    <hr class="sidebar-divider my-0">
+
+    <li class="nav-item">
+        <a class="nav-link" href="/dashboard">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+
+    <hr class="sidebar-divider">
+
+
+    <div class="sidebar-heading">
+        Edit Content
+    </div>
+
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="/index" data-toggle="collapse" data-target="#collapseOne"
+            aria-expanded="true" aria-controls="collapseOne">
+            <i class="fa fa-clone"></i>
+            <span>Add News</span>
+        </a>
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Add News</h6>
+                <a class="collapse-item" href="/dashboard/posts">Add Berita</a>
+            </div>
+        </div>
+    </li>
+
+
+    <hr class="sidebar-divider">
+
+    <div class="sidebar-heading">
+        Addons
+    </div>
+
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+            aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Menu Navigasi</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Login Screens:</h6>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button class="collapse-item custom-button" type="submit">Logout</button>
+                </form>
+                <h6 class="collapse-header">Other Pages:</h6>
+                <a class="collapse-item" href="/">Back To Beranda</a>
+                <div class="collapse-divider"></div>
+            </div>
+        </div>
+    </li>
+
+
+    <hr class="sidebar-divider d-none d-md-block">
+
+
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+
+
+</ul>
