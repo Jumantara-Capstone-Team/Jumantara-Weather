1f9ffb2f9b541bc5aaa211850ad97a419e91cc70
zesika salsa <zesikazahara@gmail.com>
2023-11-16 20:53:29 +0700

    penambahan halaman News - Salsa

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -1,16 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-<title>News | Info Cuaca</title>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>News</title>
 
-@include('components.head.head')
-@include('components.navbar.navbar')
+    <!-- CSS Utama -->
+    <link rel="stylesheet" href="asset/CSS/app.css" />
+    <link rel="icon" href="asset/IMAGE/favicon.png" />
+    <!-- Font Awesome 5.15.4 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+  </head>
+  <body>
+    <header class="navbar-container">
+      <div class="logo">
+        <img src="asset/IMAGE/favicon.png" alt="Logo Utama" />
+      </div>
+      <nav class="nav-list">
+        <ul>
+          <li><a href="/">Weather</a></li>
+          <li><a href="/">News</a></li>
+          <li><a href="/">Radar</a></li>
+          <li><a href="/">Air Quality</a></li>
+        </ul>
+      </nav>
+    </header>
 
-<body>
+    <main>
+      <!-- sidebar -->
+      <article>
+        <section>
+          <div class="aside">
+            <div class="search__container">
+              <p class="search__title">
+                Cari Berita Lainnya
+              </p>
+              <input class="search__input" type="text" placeholder="Search">
+            </div>
+          </div>
+        </section>
+      </article>
+      <!-- sidebar -->
 
+      <!-- Content -->
+      <article>
+        <section id="Beranda">
+          <div class="content">
+            <div class="content-description">
+              <h1 class="title">XYZ</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel nobis inventore praesentium aspernatur odio aut architecto aperiam quo animi, dignissimos velit consectetur corporis ducimus tempore, vero alias dolor minus harum?
+              </p>
+              <button>Lebih lanjut</button>
+            </div>
+          </div>
+        </section>
+      </article>
+      <!-- Content -->
 
-</body>
-@include('components.footer.footer')
-
-<script src="{{ asset('js/main.js') }}"></script>
-
+      <!-- Footer -->
+      <footer>
+        <p>&#169; 2023</p>
+      </footer>
+  </body>
 </html>
