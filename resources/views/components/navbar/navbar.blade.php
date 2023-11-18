e338606a9ee58922edb9e834a851145dc83270f9
Zildan <muhamadzildan13@gmail.com>
2023-11-18 10:35:35 +0700

    "edit navbar"

--- resources/views/components/navbar/navbar.blade.php
+++ resources/views/components/navbar/navbar.blade.php
@@ -1,6 +1,26 @@
 <header id="header" class="header fixed-top">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-        <a href="/" class="logo d-flex align-items-center">
+    <div class="container-navbar">
+        <a href="/" class="branding">
+            <img src="{{ asset('img/main/logo.png') }}" alt="" style="max-height: 40px; margin-right: 10px;">
+        </a>
+
+        <div class="main-navbar">
+            <button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
+			<ul class="menu">
+                <li class="nav-link"><a href="/">Beranda</a></li>
+                <li class="nav-link"><a href="/Weather">Cuaca</a></li>
+                <li class="nav-link"><a href="/berita">Berita</a></li>
+                <li class="nav-link"><a href="/Radar">Radar</a></li>
+                <li class="nav-link"><a href="/Air-Quality">Air Quality</a></li>
+            </ul>
+        </div>
+
+        <div class="mobile-navbar"></div>
+
+    </div>
+</header>
+<!-- <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+         <a href="/" class="logo d-flex align-items-center">
             <img src="{{ asset('img/main/logo.png') }}" alt="" style="max-height: 40px; margin-right: 10px;">
         </a>
         <nav id="navbar" class="navbar">
@@ -13,5 +33,4 @@
             </ul>
             <i class="bi bi-list mobile-nav-toggle" id="mobileNavToggle"></i>
         </nav>
-    </div>
-</header>
+    </div>  -->
