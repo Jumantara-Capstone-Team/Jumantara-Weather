a72a646cb19a7497d7bed57edf9e912267535da0
Syaiful Imanudin <yusriltc@gmail.com>
2023-11-20 18:42:32 +0700

    edit main hero and add logo

--- resources/views/components/beranda/hero/hero.blade.php
+++ resources/views/components/beranda/hero/hero.blade.php
@@ -1,5 +1,9 @@
 <div class="hero">
+    <div class="hero-content container">
+        <h1>Jumantara Weather Website</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, sapiente.</p>
+    </div>
     <div class="hero-image container">
-        <img src=".\img\hero\hero.png">
+        <img src="{{ asset('/img/hero/hero.png') }}" alt="Hero Image">
     </div>
 </div>
