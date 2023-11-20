a408b88e28f1d439bc5dff40747bb6f28b708805
Syaiful Imanudin <yusriltc@gmail.com>
2023-11-20 18:50:38 +0700

    membereskan dan meripihkan news

--- resources/views/components/beranda/news/news.blade.php
+++ resources/views/components/beranda/news/news.blade.php
@@ -5,7 +5,7 @@
     <div class="news-container">
 
         <div class="news-card">
-            <img class="news-img" src="{{ asset('img/contoh.jpeg') }}" alt="Gambar Berita 1"> 
+            <img class="news-img" src="{{ asset('img/contoh.jpeg') }}" alt="Gambar Berita 1">
             <div class="news-content">
             <div class="news-title">Berita 1</div>
                 <p>Ini adalah isi berita kedua.</p>
@@ -30,47 +30,53 @@
     </div>
 </section> -->
 <section id="news" class="news">
-				<div class="fullwidth-block">
-					<div class="container">
-						<h2 class="section-title">Recently News</h2>
-						<div class="row">
-							<div class="col-md-3 col-sm-6">
-								<div class="live-camera">
-									<figure class="live-camera-cover"><img src="{{ asset('img/contoh.jpeg') }}" alt=""></figure>
-									<div class="news-content-card">
-										<h3 class="location">Berita Satu</h3>
-										<small class="date">8 oct, 8:00AM</small>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-3 col-sm-6">
-								<div class="live-camera">
-									<figure class="live-camera-cover"><img src="{{ asset('img/contoh.jpeg') }}" alt=""></figure>
-									<div class="news-content-card">
-										<h3 class="location">Berita Satu</h3>
-										<small class="date">8 oct, 8:00AM</small>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-3 col-sm-6">
-								<div class="live-camera">
-									<figure class="live-camera-cover"><img src="{{ asset('img/contoh.jpeg') }}" alt=""></figure>
-									<div class="news-content-card">
-										<h3 class="location">Berita Satu</h3>
-										<small class="date">8 oct, 8:00AM</small>
-									</div>
-								</div>
-							</div>
-                            <div class="col-md-3 col-sm-6">
-								<div class="live-camera">
-									<figure class="live-camera-cover"><img src="{{ asset('img/contoh.jpeg') }}" alt=""></figure>
-									<div class="news-content-card">
-										<h3 class="location">Berita Satu</h3>
-										<small class="date">8 oct, 8:00AM</small>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+    <div class="fullwidth-block">
+        <div class="container">
+            <h2>Recently News</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque temporibus
+                excepturi autem et quam corrupti culpa non fugiat aliquam facilis!</p>
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="live-camera">
+                        <figure class="live-camera-cover"><img src="{{ asset('img/contoh.jpeg') }}" alt="">
+                        </figure>
+                        <div class="news-content-card">
+                            <h3 class="location">Berita Satu</h3>
+                            <small class="date">8 oct, 8:00AM</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="live-camera">
+                        <figure class="live-camera-cover"><img src="{{ asset('img/contoh.jpeg') }}" alt="">
+                        </figure>
+                        <div class="news-content-card">
+                            <h3 class="location">Berita Satu</h3>
+                            <small class="date">8 oct, 8:00AM</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="live-camera">
+                        <figure class="live-camera-cover"><img src="{{ asset('img/contoh.jpeg') }}" alt="">
+                        </figure>
+                        <div class="news-content-card">
+                            <h3 class="location">Berita Satu</h3>
+                            <small class="date">8 oct, 8:00AM</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6">
+                    <div class="live-camera">
+                        <figure class="live-camera-cover"><img src="{{ asset('img/contoh.jpeg') }}" alt="">
+                        </figure>
+                        <div class="news-content-card">
+                            <h3 class="location">Berita Satu</h3>
+                            <small class="date">8 oct, 8:00AM</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
