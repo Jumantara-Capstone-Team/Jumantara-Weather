c03dce49372ff9cdbb89c246bae3bcf3d171f29e
zesika salsa <zesikazahara@gmail.com>
2023-11-22 06:24:28 +0700

    penambahan fitur isi berita

--- resources/views/content/news/isi.blade.php
+++ resources/views/content/news/isi.blade.php
@@ -1,1 +1,81 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News | Jumantara</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2;
+        margin: 0;
+        padding: 0;
+      }
+      .container {
+        width: 80%;
+        margin: 20px auto;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        padding: 20px;
+        text-align: center;
+        box-sizing: border-box;
+      }
+      h1 {
+        color: #333;
+        font-size: 36px;
+        margin-top: 20px;
+        margin-bottom: 10px;
+      }
+      img {
+        max-width: 100%;
+        height: auto;
+        margin-bottom: 10px;
+      }
+      .author-date {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        gap: 2em;
+      }
+      .author,
+      .date {
+        color: #999;
+        font-size: 14px;
+      }
+      p {
+        color: #666;
+        font-size: 18px;
+        line-height: 1.5;
+        margin-bottom: 20px;
+        text-align: justify;
+      }
 
+      @media only screen and (max-width: 600px) {
+      .container {
+        width: 90%;
+        padding: 20px;
+      }
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Kucing Lucu Hari Ini</h1>
+      <img src="https://placekitten.com/800/400" alt="Gambar Berita" />
+      <div class="author-date">
+        <p class="author">Author: John Doe</p>
+        <p class="date">Tanggal: 17 November 2023</p>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint autem molestiae tenetur nostrum ex! Vitae voluptate molestiae, vero modi praesentium debitis repellendus voluptates, enim, ullam voluptatibus sint veritatis
+        optio? Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt id, cupiditate pariatur similique recusandae excepturi ducimus! Optio ipsum voluptate facere deleniti asperiores provident quia inventore officiis, obcaecati
+        tempora, cum qui? Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias omnis architecto libero distinctio quisquam, enim dolorem ipsa soluta, sed magni quia ipsum quam accusamus ut impedit asperiores veritatis dicta. 
+        Voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rem quis culpa similique ipsa autem quas sapiente natus inventore. Delectus, possimus. Ipsa cumque totam, consectetur iste libero a id praesentium. Lorem 
+        ipsum dolor sit amet consectetur adipisicing elit. Incidunt inventore nulla molestiae explicabo architecto quas! Ullam nobis eius odit non? Excepturi omnis ex eius adipisci nisi dolorem facilis. Cupiditate, dolore? Lorem ipsum dolor 
+        sit amet consectetur adipisicing elit. Et cupiditate doloribus architecto nihil, ratione magnam temporibus, officiis porro commodi voluptate harum. Odio a nisi error repellendus quos at dolor quas. Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Eveniet nemo ipsa aperiam quia consequuntur reiciendis vero, rerum inventore alias exercitationem, recusandae esse. Ex quasi, laboriosam rem eum cupiditate quidem accusamus?
+      </p>
+    </div>
+  </body>
+</html>
