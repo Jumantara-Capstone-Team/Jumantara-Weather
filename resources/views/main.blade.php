53c21d7339c70c442b43acef21c38fa582ca6431
Syaiful Imanudin <yusriltc@gmail.com>
2023-11-22 15:49:23 +0700

    delete command

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,8 +2,6 @@
 <html lang="en">
 
 @include('components.head.head')
-{{-- -> @include berguna untuk menyisipkan file view untuk ke dalam halaman kita,
-untuk yg di maksud components.head.head adalah dimana letak sebuah file nya dan di akhiri oleh nama file nya yaitu head --}}
 
 @include('components.navbar.navbar')
 
