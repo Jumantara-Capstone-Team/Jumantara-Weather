4a0816fbde668404a96b1e04fbeae0e3dc62fe5f
Zildan <muhamadzildan13@gmail.com>
2023-12-09 20:24:37 +0700

    add search bar di weather

--- resources/views/content/weather/weather.blade.php
+++ resources/views/content/weather/weather.blade.php
@@ -7,8 +7,10 @@
 @include('components.navbar.navbar')
 
 <body class="mt-20">
-<div class="min-h-screen flex items-center justify-center mx-12">
-<div class="flex flex-col bg-white rounded p-4 w-full max-w-xs">
+<section class="bg-gray-50 dark:bg-gray-800 bg-gradient-to-b from-green-200 to-sky-300">
+    <div class="max-w-screen-xl px-4 py-8 mx-auto space-y-12 lg:space-y-20 lg:py-24 lg:px-6">
+    <div class="font-bold text-4xl text-white text-center">Temukan Suhu Harian di Lokasi Anda</div>
+        <!-- <div class="flex flex-col bg-white rounded p-4 w-full max-w-xs">
 						<div class="font-bold text-xl">Sydney</div>
 						<div class="text-sm text-gray-500">Thursday 10 May 2020</div>
 						<div class="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
@@ -42,53 +44,19 @@
 								<div class="text-sm text-gray-500">10km</div>
 							</div>
 						</div>
-					</div>
+					</div> -->
+        <div class="bg-white items-center justify-between w-full flex rounded-full shadow-lg p-2 mb-5" style="top: 5px ">
+        <input class="font-bold rounded-full w-full py-4 pl-4 text-gray-700 bg-gray-100 leading-tight focus:outline-none focus:shadow-outline lg:text-sm text-xs" type="text" placeholder="Cari Lokasi">
 
-    <div class="container mx-auto pl-4 py-8">
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3">
-            <div class="bg-white rounded shadow p-4">
-                <h2 class="text-xl font-semibold mb-2">Tuesday</h2>
-                <div class="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
-                        <svg class="w-32 h-32" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"></path></svg>
-						</div>
-                <p class="text-gray-600 text-center">Partly cloudy</p>
-                <p class="text-gray-600 text-center">25°C</p>
-            </div>
-            <div class="bg-white rounded shadow p-4">
-                <h2 class="text-xl font-semibold mb-2">Wednesday</h2>
-                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
-                <p class="text-gray-600 text-center">Sunny</p>
-                <p class="text-gray-600 text-center">28°C</p>
-            </div>
-            <div class="bg-white rounded shadow p-4">
-                <h2 class="text-xl font-semibold mb-2">Thursday</h2>
-                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
-                <p class="text-gray-600 text-center">Cloudy</p>
-                <p class="text-gray-600 text-center">24°C</p>
-            </div>
-            <div class="bg-white rounded shadow p-4">
-                <h2 class="text-xl font-semibold mb-2">Friday</h2>
-                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
-                <p class="text-gray-600 text-center">Partly cloudy</p>
-                <p class="text-gray-600 text-center">23°C</p>
-            </div>
-            <div class="bg-white rounded shadow p-4">
-                <h2 class="text-xl font-semibold mb-2">Saturday</h2>
-                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
-                <p class="text-gray-600 text-center">Sunny</p>
-                <p class="text-gray-600 text-center">27°C</p>
-            </div>
-            <div class="bg-white rounded shadow p-4">
-                <h2 class="text-xl font-semibold mb-2">Sunday</h2>
-                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
-                <p class="text-gray-600 text-center">Partly cloudy</p>
-                <p class="text-gray-600 text-center">25°C</p>
+            <div class="bg-gray-600 p-2 hover:bg-blue-400 cursor-pointer mx-2 rounded-full">
+
+                <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                </svg>
             </div>
         </div>
-    </div>
-</div>
-<div class="container mx-auto px-4">
-        <div class="bg-white rounded shadow p-4">
+    <div class="container mx-auto">
+        <div class="bg-white rounded-lg shadow-lg p-4">
             <h1 class="text-2xl font-bold mb-4">Today's Weather</h1>
             <div class="flex justify-between items-center">
                 <div class="text-5xl font-bold mb-4">12°C</div>
@@ -111,6 +79,50 @@
             </div>
         </div>
     </div>
+    <div class=" mx-auto py-8 my-auto">
+        <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3">
+            <div class="bg-white rounded shadow-lg p-4">
+                <h2 class="text-xl font-semibold mb-2 text-center">Tuesday</h2>
+                    <div class="items-center  rounded-lg text-indigo-400 h-24 w-24">
+                        <svg class="w-32 h-32 items-center" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"></path></svg>
+				    </div>
+                <p class="text-gray-600 text-center">Partly cloudy</p>
+                <p class="text-gray-600 text-center">25°C</p>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg p-4">
+                <h2 class="text-xl font-semibold mb-2 text-center">Wednesday</h2>
+                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
+                <p class="text-gray-600 text-center">Sunny</p>
+                <p class="text-gray-600 text-center">28°C</p>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg p-4">
+                <h2 class="text-xl font-semibold mb-2 text-center">Thursday</h2>
+                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
+                <p class="text-gray-600 text-center">Cloudy</p>
+                <p class="text-gray-600 text-center">24°C</p>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg p-4">
+                <h2 class="text-xl font-semibold mb-2 text-center">Friday</h2>
+                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
+                <p class="text-gray-600 text-center">Partly cloudy</p>
+                <p class="text-gray-600 text-center">23°C</p>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg p-4">
+                <h2 class="text-xl font-semibold mb-2 text-center">Saturday</h2>
+                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
+                <p class="text-gray-600 text-center">Sunny</p>
+                <p class="text-gray-600 text-center">27°C</p>
+            </div>
+            <div class="bg-white rounded-lg shadow-lg p-4">
+                <h2 class="text-xl font-semibold mb-2 text-center">Sunday</h2>
+                <img src="https://via.placeholder.com/50" alt="Weather Icon" class="mx-auto mb-4">
+                <p class="text-gray-600 text-center">Partly cloudy</p>
+                <p class="text-gray-600 text-center">25°C</p>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
 </body>
 
 @include('components.footer.footer')
