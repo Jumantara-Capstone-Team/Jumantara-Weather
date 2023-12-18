62af57d8948b074b095571f874368b8607938c34
Syaiful Imanudin <112718167+SaipulImdn@users.noreply.github.com>
2023-12-18 15:04:41 +0700

    Update head.blade.php
    edit head fix

--- resources/views/components/head/head.blade.php
+++ resources/views/components/head/head.blade.php
@@ -3,6 +3,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+     <meta name="dicoding:email" content="muhamadzildan13@gmail.com">
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <lin href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,700;1,9..40,100;1,9..40,400;1,9..40,500;1,9..40,700&display=swap"
